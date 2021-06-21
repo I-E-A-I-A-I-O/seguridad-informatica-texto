71e174eb9c3d78c7949e952bb4c390988cac7245
justdancepc <jesusdavid268@outlook.com>
2021-06-21 19:39:28 -0400

    try catch and log messages on file upload endpoint

--- server/dev/Controllers/Controller.ts
+++ server/dev/Controllers/Controller.ts
@@ -9,10 +9,19 @@ export const getInstaller = async (req: Request, res: Response, next: NextFuncti
 export const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
     const files = req.files as Express.Multer.File[];
     if (!files) {
+	console.error("Received request with no file input");
         return res.sendStatus(400);
     }
-    for (let i = 0; i < files.length; i++) {
-        await fse.copy(files[i].path, `textFiles/${files[i].originalname}`);
+    try {
+	console.info("Processing received files");
+        for (let i = 0; i < files.length; i++) {
+	    console.info(`Copying file ${files[i].originalname}.`);
+            await fse.copy(files[i].path, `textFiles/${files[i].originalname}`);
+	    console.info(`file ${files[i].originalname} copied.`);
+        }
+        res.sendStatus(200);
+    } catch (err) {
+	console.error(err);
+	res.sendStatus(500);
     }
-    res.sendStatus(200);
 };
