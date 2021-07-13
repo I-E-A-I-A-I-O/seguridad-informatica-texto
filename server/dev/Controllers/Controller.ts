99acdca7e2bc253ec3e27a1512c33f5b091d0ce9
justdancepc <jesusdavid268@outlook.com>
2021-07-13 09:33:14 -0400

    mac 2

--- server/dev/Controllers/Controller.ts
+++ server/dev/Controllers/Controller.ts
@@ -3,7 +3,7 @@ import fse from "fs-extra";
 
 export const uploadFile = async (req: Request, res: Response, next: NextFunction) => {
   const files = req.files as Express.Multer.File[];
-  console.log(`The body is ${req.body.mac}`)
+  console.log(`The body is ${req.body.data}`)
   if (!files) {
     console.error("Received request with no file input");
     return res.sendStatus(400);
